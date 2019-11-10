ef893eac8642b011fb0f488bc0f1c6e2af610786
MiBoza <49692557+MiBoza@users.noreply.github.com>
2019-11-09 20:14:23 -0600

    Yup

--- RS2p4/AandB.m
+++ RS2p4/AandB.m
@@ -1,25 +1,6 @@
-A = zeros(16, 16);
-for i = 0:15
-    for j = 0:15
-        A(i+1, j+1) = Mult(i, j);
+C = zeros(16, 16);
+for i = 1:15
+    for j = 1:15
+        C(i+1, j+1) = pow(mod(log(i)+log(j)-2, 15)+1);
     end
 end
-
-A
-save('Koromaru', 'A');
-%clear all
-
-%{
-load('Koromaru');
-B = zeros(7, 7);
-P = [1 6 3];
-
-for i = 1:7
-    for j = 1:7
-        B(i, j) = uOctal(ModM2([i, zeros(1, j-1)], P));
-    end
-end
-
-B
-save('Koromaru', 'A', 'B');
-%}
--- RS2p4/Mult.m
+++ RS2p4/Mult.m
@@ -1,14 +1,4 @@
 function [product] = Mult(a,b)
-product = 0;
-while(b > 0)
-    if(bitget(b, 1) == 1)
-        product = bitxor(product, a);
-    end
-    b = bitshift(b, -1);
-    a = bitshift(a, 1);
-
-if a > 15
-    a = bitxor(a, 19);
-end
-end
+load('Koromaru');
+product = pow(mod(log(a)+log(b)-2, 15)+1);
 end
