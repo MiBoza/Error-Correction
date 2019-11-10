8e9201d1d49ccc227126291b1c4afc3cee50d49f
byOverShadows <andyc1395@gmail.com>
2019-11-09 20:24:58 -0600

    Update ruido.m
    ya funciona

--- Tests/ruido.m
+++ Tests/ruido.m
@@ -1,5 +1,7 @@
-%[x, fs] = audioread('Battle for Everyones Souls.wav');
-sound(x(1:1000000, :), fs);
+clear all
+load handel.mat
+
+[x2, fs] = audioread('hola.wav');
 t = 0:1/fs:1;
 x = cos(2*pi*250*t) + 0.8*cos(2*pi*500*t) + 0.4*cos(2*pi*750*t);
 
@@ -8,5 +10,8 @@ L = length(t);
 var = 2;   % varianza
 k = randn(1,L);   % ruido no uniforme
 mean1 = 0.0;    % media
-ruido1 = mean1 + sqrt(var)*k;   % ruido blanco
-ruido1 = awgn(x,20);
+ruido1 = awgn(x2,50);
+x1=ruido1;
+in = 1:100;
+plot(in, x(1:100),in, x1(1:100));
+sound(x1,fs)
