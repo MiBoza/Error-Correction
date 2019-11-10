ee72b16ab309ed21680c5b3850dbfabe9bf94dff
MiBoza <49692557+MiBoza@users.noreply.github.com>
2019-11-09 20:39:35 -0600

    Yup

--- Tests/ruido.m
+++ Tests/ruido.m
@@ -1,7 +1,4 @@
-clear all
-load handel.mat
-
-[x2, fs] = audioread('hola.wav');
+[x2, fs] = audioread('Battle for Everyones Souls.wav');
 t = 0:1/fs:1;
 x = cos(2*pi*250*t) + 0.8*cos(2*pi*500*t) + 0.4*cos(2*pi*750*t);
 
@@ -10,7 +7,7 @@ L = length(t);
 var = 2;   % varianza
 k = randn(1,L);   % ruido no uniforme
 mean1 = 0.0;    % media
-ruido1 = awgn(x2,50);
+ruido1 = awgn(x2,10);
 x1=ruido1;
 in = 1:100;
 plot(in, x(1:100),in, x1(1:100));
